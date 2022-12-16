3bc6ea7d01de748ddaae58b199f0d66a26e38e62
SHaripov KHasan <iamxasann@gmail.com>
2022-12-16 14:05:46 +0500

    xexesalom calculator

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("iltimos amallar orasida bosh joy tashlab yozing");
+var mathematics = Console.ReadLine().Split(" ");
+int x = int.Parse(mathematics[0]);
+int y = int.Parse(mathematics[2]);
+int z = x + y;
+int q = x - y;
+int w = x * y;
+int e = x / y;
+string symbol = mathematics[1];
+
+if (symbol == "+")
+{
+    Console.WriteLine($"{x} va {y} yigindisi {z}");    
+}
+
+else if (symbol == "-")
+{
+    Console.WriteLine($"{x} va {y} ayirmasi {q}");
+}
+
+else if (symbol == "*")
+{
+    Console.WriteLine($"{x} va {y} kopaytmasi {w}");
+}
+
+else if (symbol == "/")
+{
+    Console.WriteLine($"{x} va {y} bo`linmasi {e}");
+}
+
+else
+    Console.WriteLine("xatolik iltimos boshidan kiriting");
